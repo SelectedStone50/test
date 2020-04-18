83770081e406b045d02680336be6588e82b8ca71
SelectedStone50 <37983589+SelectedStone50@users.noreply.github.com>
2020-04-18 14:27:08 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const hbs = require('hbs');
+const covid = require('./utils/covid'); 
+const news = require('./utils/news');
+var cors = require('cors');
+
+const app = express();
+const PORT = process.env.PORT || 3000; 
+
+// Public folder
+app.use(express.static('public'));
+app.use(cors());
+
+// Handlebars setup
+app.set('view engine', 'hbs');
+
+app.get('/', function(req, res){
+    res.render('index'); 
+});
+
+app.get('/covid', function(req, res){
+    covid((error, data) => {
+        res.send(data); 
+    }); 
+}); 
+
+app.get('/news', function(req, res){
+    let country = req.query.country; 
+    if(country){
+        news(country, (error, data) => {
+            res.send(data);
+        }); 
+    }else{
+        res.send({
+            error: "Please provide a country."
+        }); 
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`Listening on PORT ${PORT}`);
+});
